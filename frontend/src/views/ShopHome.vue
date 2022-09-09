41d75bd097b9a422b8acee5904bb6936ca143e9b
Your NamesadligC <gildas.c@ik.me>
2022-09-09 11:35:36 +0200

    structure changes: add  CmsHome.vue & ShopHome.vue, changes to router index.js

---
+++ frontend/src/views/ShopHome.vue
@@ -0,0 +1,30 @@
+
+<template>
+
+    <HeaderShop></HeaderShop>
+    <NavbarShop></NavbarShop>
+    <router-view></router-view>
+    <FooterShop></FooterShop>
+   
+</template>
+  
+<script>
+    import HeaderShop from "../components/HeaderShop.vue";
+    import NavbarShop from "../components/NavbarShop.vue";
+    import FooterShop from "@/components/FooterShop.vue";
+  
+    export default {
+        name: 'ShopHome',
+        components: {
+            HeaderShop,
+            NavbarShop,
+            FooterShop
+        }
+  }
+</script>
+  
+<style>
+  
+</style>
+  
+  
