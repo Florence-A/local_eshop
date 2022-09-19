871535749ad4f7afedd8b405beb75e4b2ca6014a
sadligC <gildas.c@ik.me>
2022-09-19 18:41:23 +0200

    product controller new product: bug on feature checking

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,7 @@ module.exports = {
         // create a product from params
         const newProduct = {
             name            : req.body.name,
-            _ref            : req.body.ref,
+            _ref            : `_ref_${Math.round(Math.random()*1000)}`, //random generated number for test... todo: créate a generating function
             description     : req.body.description,
             HT_price        : req.body.HT_price,
             lead_time       : req.body.lead_time,
@@ -23,9 +23,11 @@ module.exports = {
         
         const productParams = {
             img_path        : req.body.img_path, //just passing a path... will be an image upload with multiple actions - gildas - 15/09
-            categories      : [{ catParent: req.body.parentCategory_id }, { catChild: req.body.childCategory_id }],
-            features        : [{ feature: req.body.feature1_id, feature_value: req.body.feature1_value_id }, { feature: req.body.feature2_id, feature_value: req.body.feature2_value_id }]
+            categories      : [],
+            features        : []
         }
+
+        const paramFeat = [{ feature: req.body.feature1_id, feature_value: req.body.feature1_value_id }, { feature: req.body.feature2_id, feature_value: req.body.feature2_value_id }]
        
     // check params before writing the newProduct in db
     // call to await function so the program waits for the checks are done before trying to write the bd
@@ -33,64 +35,51 @@ module.exports = {
 
         
     async function checkParams() {
-        
-        await sequelize.transaction(async(t)=>{
-
-       
+               
         // is tva valid
         await models.Tva.findByPk(
-            newProduct.tva_id,
-            { tansaction: t }
+            newProduct.tva_id
         ).then(tvaFound =>{
             if( tvaFound ){
                 return
             }
             throw( error = "parameter value doesn't match" )
-            // res.status(400).json({ "error" : "tva doesn't exist" })
-            // return paramErr = "parameter value doesn't match"
-        })
-        .catch(err =>{ 
-            // res.status(501).json({ "cannot find tva: " : "" + err })
-            throw( error = err )
         })
 
         // is category valid
         await models.Category.findByPk(
-            productParams.categories[0].catParent
+            req.body.parentCategory_id
         ).then(async parentCategoryFound =>{
             if( parentCategoryFound ){
                 
-                const childCategories = await parentCategoryFound.getCategories()
-                
+                const childCategories = await parentCategoryFound.getChild_category();
+               
                 await models.Category.findByPk(
-                    productParams.categories[1].catChild
+                    req.body.childCategory_id
                 ).then(childCategoryFound =>{
+
                     if( childCategoryFound ){
-                    // console.log(childCategories)
+
                         for( category of childCategories ){
-                            
                             if( category.dataValues.id === childCategoryFound.dataValues.id ){
                                 return
                             }
                         }
-                        throw( error = "child category doesn't belong to parent category" )    
+                        throw error = "child category doesn't belong to parent category" 
                         
                     }
-                    throw( error= "parameter value doesn't match" )
-                }).catch(err =>{ 
-                    throw( error = err )  
-                })
-
-            } else {
-                throw( error = err )
-            }
+                    throw error= "parameter value doesn't match" 
             
-        }).catch(err =>{ 
-            throw( error = err )
+                })
+                
+            } else {
+                throw error = "parameter value doesn't match"
+            }
         })
         
         // are features valid
-        for( feature of productParams.features ){
+        for( feature of paramFeat ){
+            console.log(feature); 
             await models.Feature.findByPk(
                 feature.feature
             ).then(async featureFound =>{
@@ -102,41 +91,60 @@ module.exports = {
                         feature.feature_value
                     ).then(feature_valueFound =>{
                         if( feature_valueFound ){
-                            console.log(feature_valueFound.dataValues.value)
+                            
                             for( element of featureValues ){
                                 if( element.dataValues.id === feature_valueFound.dataValues.id ){
-                                    console.log(element.dataValues.id)
-                                    console.log(feature_valueFound.dataValues.id)
+                                    productParams.features.push(feature_valueFound.dataValues.id)
                                     return
                                 }
-                                res.status(400).json({ "error" : "feature value doesn't belong to feature" })
-                                return paramErr = "feature value doesn't belong to feature"
+                                throw error = "feature value doesn't belong to feature";
                             }
-                            res.status(400).json({ "error" : "feature value doesn't exist" })
-                            return paramErr = "parameter value doesn't match"
+                            throw error = "parameter value doesn't match"
                         }
-                    }).catch(err =>{ 
-                        res.status(501).json({ "cannot find feature_value: " : "" + err })
                     })
 
                 }else {
-                    res.status(400).json({ "error" : "feature doesn't exist" })
-                    return paramErr = "parameter value doesn't match"
+                    throw error = "parameter value doesn't match"
                 }
-            }).catch(err =>{ 
-                res.status(501).json({ "cannot find feature: " : "" + err })
             })
         }
 
-        });
     }
     
     try {
-        await checkParams();
-        if(typeof paramErr === 'undefined' ) {console.log(newProduct)}
-        else {console.log(paramErr)}
+        await checkParams()
+
+        await sequelize.transaction(async(t)=>{
+            await models.Product.create(
+                newProduct,
+                {  tansaction: t }
+            ).then(async productCreated =>{
+                
+                productCreated.addCategories(
+                    productParams.categories
+                )
+
+                productCreated.addFeature_values(
+                    productParams.features
+                )
+
+                await sequelize.transaction(async(t)=>{
+                    await models.Image.create(
+                        { path: productParams.img_path },
+                        { tansaction: t }
+                    ).then(imageCreated =>{
+                        
+                        productCreated.addImage(
+                            imageCreated
+                        )
+                    })
+                });
+
+            })
+        })
+        
     } catch (err) {
-        console.log(err)
+        res.status(500).json({"error": "" + err})
     }
     
 
@@ -189,13 +197,12 @@ module.exports = {
                 attributes: ['path']
             }
         ]
+        }).then(productsFound => {
+            return res.status(201).json(productsFound)
         })
-            .then(productsFound => {
-                return res.status(201).json(productsFound)
-            })
-            .catch(err => {
-                return res.status(500).json({ 'error': 'cannot find products: ' + err})
-            })
+        .catch(err => {
+            return res.status(500).json({ 'error': 'cannot find products: ' + err})
+        })
     },
 
     // get one product with associated attributes by pk
